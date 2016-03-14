e35428f97726aea22c948a2f55e218a9457d275a
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-14 17:15:23 -0400

    last changes at campus

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+
+import data from './data';
+
+
+import { text, email, select, textarea, tele } from './template';
+
+
+
+var dataTypeToTemplate = {
+  'Manager': manager,
+  'Employee': employee,
+  'Customer': customer
+}
---
+++ src/js/template.js
@@ -0,0 +1,19 @@
+export function text(user){
+	
+}
+
+export function email(user){
+	
+}
+
+export function select(user){
+	
+}
+
+export function textarea(user){
+	
+}
+
+export function tele(user){
+	
+}
